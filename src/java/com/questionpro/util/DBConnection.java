102cab1ceed8c4f5bd272a7911e43165810f79f0
nagaarjun93 <nagaarjunn31@gmail.com>
2026-07-06 13:51:54 +0530

    Updated Railway MySQL connection

--- src/java/com/questionpro/util/DBConnection.java
+++ src/java/com/questionpro/util/DBConnection.java
@@ -7,33 +7,20 @@ import java.sql.SQLException;
 public class DBConnection {
 
     private static final String URL =
-            "jdbc:mysql://localhost:3306/questionpro?useSSL=false&serverTimezone=Asia/Kolkata&allowPublicKeyRetrieval=true";
+        "jdbc:mysql://hopper.proxy.rlwy.net:53256/railway?useSSL=true&serverTimezone=Asia/Kolkata";
 
     private static final String USER = "root";
-    private static final String PASSWORD = "Arjun@123";
+    private static final String PASSWORD = "BRWiWYjRBCbfSTusWBBiQmevqKkRUKWD";
 
     static {
-
         try {
-
             Class.forName("com.mysql.cj.jdbc.Driver");
-
         } catch (ClassNotFoundException e) {
-
             throw new RuntimeException("MySQL JDBC Driver Not Found", e);
-
         }
-
-    }
-
-    private DBConnection() {
-
     }
 
     public static Connection getConnection() throws SQLException {
-
         return DriverManager.getConnection(URL, USER, PASSWORD);
-
     }
-
 }
